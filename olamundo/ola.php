66d01e0502bed6c9b85d79c859226c2510bd7b8c
Maicon Cerutti <maicon.cerutti@univille.br>
2017-11-19 22:39:50 -0200

    Aula 03 - Criando ambiente + Olá Mundo!

---
+++ olamundo/ola.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Primeiro exemplo PHP</title>
+    <style>
+        h2 {
+            color: #297a79;
+            text-shadow: 1px 1px 1px black;
+        }
+    </style>
+</head>
+<body>
+    <h1> Testando PHP</h1>
+<?php
+    echo "<h2>Olá Mundo!</h2>";
+
+
+?>
+</body>
+
+
+
+
+
+</html>
