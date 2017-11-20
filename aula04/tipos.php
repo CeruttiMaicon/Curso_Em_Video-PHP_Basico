4c6ddf1758f656c673ad1fc0375e9bf6645d3a1c
Maicon Cerutti <maicon.cerutti@univille.br>
2017-11-20 09:41:21 -0200

    Aula 04 - Variaveis

---
+++ aula04/tipos.php
@@ -0,0 +1,23 @@
+<!Doctype html>
+<html>
+
+
+<head>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="_css/estilo.css"
+    <title>Aula054</title>
+
+</head>
+<body>
+<div>
+    <?php
+        $idade = 36;
+        $nome = (string)"Gustavo";
+        echo "$nome  tem $idade anos";
+
+    ?>
+</div>
+
+</body>
+</html>
+
