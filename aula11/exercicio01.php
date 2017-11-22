5b52bdd862da18f069d3ddec46ac87f01371f2e0
Maicon Cerutti <maicon.cerutti@univille.br>
2017-11-22 17:07:16 -0200

    Aula 11 + Assisti as outras aulas e todos os comandos são parecidos ou iguais ao Python então não realizei mais atividades e fui direto para a prova + CERTIFICADO

---
+++ aula11/exercicio01.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+
+        $c= 0;
+        while($c<=10){
+            echo "$c " ;
+            $c++;
+        }
+
+        
+    ?>
+</div>
+</body>
+</html>
+ 
